2e7d9e8f02bfad59a2cf1bdf0550270649639b9a
Denis Zyulev <zzulev@gmail.com>
2019-06-11 11:26:09 +0700

    Set name

--- main.ts
+++ main.ts
@@ -75,6 +75,7 @@ function createWindow() {
 }
 
 try {
+  app.setName('Pomodoro Cycle');
   // This method will be called when Electron has finished
   // initialization and is ready to create browser windows.
   // Some APIs can only be used after this event occurs.
